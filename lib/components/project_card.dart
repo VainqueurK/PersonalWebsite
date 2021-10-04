d559b5a3ca897f23edc3e5f2f4f3a3edaeeef472
Vainqueur <vainqueurkk@gmail.com>
2021-10-05 00:21:15 +0100

    project page progress

--- lib/components/project_card.dart
+++ lib/components/project_card.dart
@@ -11,11 +11,11 @@ class ProjectCard extends StatefulWidget {
   Tags tags;
   ProjectCard({
     Key? key,
-    this.fontColor = Colors.white,
+    this.fontColor = Colors.black87,
     this.fontFamily = "roboto",
     this.fontSize = 24,
-    this.title = "",
-    this.text = "",
+    this.title = "N/A",
+    this.text = "N/A",
     required this.tags,
   }) : super(key: key);
 
@@ -27,21 +27,67 @@ class _ProjectCardState extends State<ProjectCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: Statics.DEVICE_WIDTH(context) * 0.14,
-      height: Statics.DEVICE_HEIGHT(context) * 0.3,
-      constraints: const BoxConstraints(
-          maxWidth: 400, maxHeight: 600, minHeight: 200, minWidth: 300),
-      decoration: BoxDecoration(
-        color: Color(0xFF2E2E2E),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Text(
-        widget.text,
-        style: TextStyle(
-            color: widget.fontColor,
-            fontSize: widget.fontSize,
-            fontFamily: widget.fontFamily,
-            fontWeight: FontWeight.w300),
+      width: Statics.DEVICE_WIDTH(context) * 0.5,
+      child: Row(
+        children: [
+          Container(
+            width: Statics.DEVICE_WIDTH(context) * 0.14,
+            height: Statics.DEVICE_HEIGHT(context) * 0.3,
+            constraints: const BoxConstraints(
+                maxWidth: 400, maxHeight: 600, minHeight: 300, minWidth: 300),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: Statics.DEVICE_WIDTH(context) * 0.017,
+          ),
+          Flexible(
+            child: Container(
+              height: Statics.DEVICE_HEIGHT(context) * 0.17,
+              constraints: BoxConstraints(minHeight: 200, minWidth: 500),
+              child: Expanded(
+                flex: 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      widget.title,
+                      overflow: TextOverflow.visible,
+                      style: const TextStyle(
+                        fontFamily: 'Rubik',
+                        fontSize: 50,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: SingleChildScrollView(
+                        child: Text(
+                          widget.text,
+                          style: TextStyle(
+                              color: widget.fontColor,
+                              fontSize: widget.fontSize,
+                              fontFamily: widget.fontFamily,
+                              fontWeight: FontWeight.w300),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/pages/projectpage.dart
+++ lib/pages/projectpage.dart
@@ -22,61 +22,109 @@ class _ProjectsPageState extends State<ProjectsPage> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: Statics.DEVICE_HEIGHT(context),
-        width: double.infinity,
-        color: Colors.grey[100], //Color(0xFF121212),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(
-              height: Statics.DEVICE_HEIGHT(context) * 0.015,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                left: 20,
-              ),
-              child: SizedBox(
-                width: Statics.DEVICE_WIDTH(context),
-                child: InkWell(
-                  onTap: () async {
-                    await context.vxNav.push(Uri.parse("/"));
-                  },
-                  onHover: (hover) {
-                    setState(() {
-                      projectsTextColor = hover ? hoverColor : normalColor;
-                    });
-                  },
-                  child: Text(
-                    "< Home",
-                    style: TextStyle(
-                      fontFamily: 'Rubik',
-                      fontSize: 50,
-                      color: projectsTextColor,
-                    ),
+            Container(
+              height: Statics.DEVICE_HEIGHT(context) * 3,
+              width: double.infinity,
+              color: Colors.grey[100], //Color(0xFF121212),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: Statics.DEVICE_HEIGHT(context) * 0.015,
                   ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.only(top: 100),
-                width: Statics.DEVICE_WIDTH(context) * 0.75,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "Projects",
-                      style: TextStyle(
-                        fontFamily: 'Rubik',
-                        fontSize: 80,
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                    ),
+                    child: InkWell(
+                      onTap: () async {
+                        await context.vxNav.push(Uri.parse("/"));
+                      },
+                      onHover: (hover) {
+                        setState(() {
+                          projectsTextColor = hover ? hoverColor : normalColor;
+                        });
+                      },
+                      child: Text(
+                        "< Home",
+                        style: TextStyle(
+                          fontFamily: 'Rubik',
+                          fontSize: 50,
+                          color: projectsTextColor,
+                        ),
                       ),
                     ),
-                    SizedBox(
-                      height: Statics.DEVICE_HEIGHT(context) * 0.05,
+                  ),
+                  Center(
+                    child: Expanded(
+                      flex: 1,
+                      child: Container(
+                        padding: EdgeInsets.only(top: 100),
+                        width: Statics.DEVICE_WIDTH(context) * 0.5,
+                        child: Column(
+                          children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: const Text(
+                                "Projects",
+                                style: TextStyle(
+                                  fontFamily: 'Rubik',
+                                  fontSize: 80,
+                                ),
+                              ),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  height: Statics.DEVICE_HEIGHT(context) * 0.05,
+                                ),
+                                ProjectCard(
+                                  title: "Heavy Machinery Rental System",
+                                  text:
+                                      "Machinery rental management tool that allows companies to easily check for Machine availability, price, specifications and make a rental for a specific period. Applied design patterns, documentation, testing, implementation, and version control usage.",
+                                  tags: Tags(),
+                                ),
+                                SizedBox(
+                                  height: Statics.DEVICE_HEIGHT(context) * 0.05,
+                                ),
+                                ProjectCard(
+                                  title:
+                                      "Vehicle Speed Detection with an Uncalibrated Camera",
+                                  text:
+                                      "Final year project, a computer vision and machine learning project to try to estimate the speed of incoming vehicles from a single video without any information on the environment or camera angles and perspective. Performing special calculations frame by frame on video inputs.",
+                                  tags: Tags(),
+                                ),
+                                SizedBox(
+                                  height: Statics.DEVICE_HEIGHT(context) * 0.05,
+                                ),
+                                ProjectCard(
+                                  title:
+                                      "Fakebook: Android Social Media Clone ",
+                                  text:
+                                      "Fakebook is a social media application that was written using java and is compatible with android versions 5 and above. The app offers an integrated messaging system where users can communicate with their friends and anybody that they have connected with on the app. The app uses Firebase to store user details along with messages by that user.",
+                                  tags: Tags(),
+                                ),
+                                SizedBox(
+                                  height: Statics.DEVICE_HEIGHT(context) * 0.05,
+                                ),
+                                ProjectCard(
+                                  title: "Supermarket Simulator",
+                                  text:
+                                      "Simulates a user selected day in a virtual supermarket under chosen weather conditions. An application for the purpose of applied learning of concurrency and threads using goroutines.",
+                                  tags: Tags(),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
